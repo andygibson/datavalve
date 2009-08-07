619197ac39bb82c1bdf340aa841772f15b4fd563
Andy Gibson <contact@andygibson.net>
2009-08-07 14:05:22 +0000

    Created the read only iterator to be returned from the iterator<t>() method.

---
+++ modules/jdataset-core/src/main/java/org/jdataset/util/DatasetIterator.java
@@ -0,0 +1,51 @@
+package org.jdataset.util;
+
+import java.util.Iterator;
+
+import org.jdataset.ObjectDataset;
+
+public class DatasetIterator<T> implements Iterator<T> {
+
+	private static final long serialVersionUID = 1L;
+	private final ObjectDataset<T> dataset;
+	private int index;
+
+	public DatasetIterator(ObjectDataset<T> dataset) {
+		super();
+		if (dataset == null) {
+			throw new IllegalArgumentException(
+					"Cannot pass null to the dataset iterator");
+		}
+		this.dataset = dataset;
+		dataset.first();
+		index = 0;
+
+	}
+
+	public boolean hasNext() {
+		if (index < dataset.getResults().size()) {
+			return true;
+		}
+
+		return dataset.isNextAvailable();
+	}
+
+	public T next() {
+		if (index == dataset.getResults().size()) {
+			dataset.next();
+			index = 0;
+		}
+		if (index < dataset.getResults().size()) {
+			return dataset.getResults().get(index++);
+		}
+
+		throw new IllegalStateException("Unable to return item");
+	}
+
+	public void remove() {
+		throw new UnsupportedOperationException(
+				"Dataset iterators do not support element removal");
+
+	}
+
+}

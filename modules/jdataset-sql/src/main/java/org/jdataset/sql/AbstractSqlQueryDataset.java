6d29b7dd7f7003b55336bf6818949092b24245d9
Andy Gibson <contact@andygibson.net>
2009-08-13 19:27:48 +0000

    Added logging and setting the parameters when we build the statement

--- modules/jdataset-sql/src/main/java/org/jdataset/sql/AbstractSqlQueryDataset.java
+++ modules/jdataset-sql/src/main/java/org/jdataset/sql/AbstractSqlQueryDataset.java
@@ -7,10 +7,13 @@ import java.sql.SQLException;
 import java.util.Collections;
 import java.util.List;
 
+import org.jdataset.Parameter;
 import org.jdataset.QueryDataset;
 import org.jdataset.db.AbstractQueryDataset;
 import org.jdataset.db.RestrictionBuilder;
 import org.jdataset.db.RestrictionBuilder.ParameterStyle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Provides the base implementation for a SQL based {@link QueryDataset} which
@@ -25,6 +28,7 @@ public abstract class AbstractSqlQueryDataset<T> extends AbstractQueryDataset<T>
 		implements ResultSetObjectFactory<T> {
 
 	private static final long serialVersionUID = 1L;
+	private static Logger log = LoggerFactory.getLogger(AbstractSqlQueryDataset.class);
 	
 	private transient Connection connection;
 	private ResultSetObjectTransformer<T> objectTransformer = new ResultSetObjectTransformer<T>();
@@ -39,13 +43,17 @@ public abstract class AbstractSqlQueryDataset<T> extends AbstractQueryDataset<T>
 
 	@Override
 	protected List<T> fetchResultsFromDatabase(Integer count) {
+		log.debug("FetchResultsFromDB, order = {}",getOrderKey());
 		PreparedStatement statement = null;
 		try {
 			statement = buildPreparedStatement(getSelectStatement(),true);
+			
 			ResultSet resultSet = statement.executeQuery();
 
-			return objectTransformer.createListFromResultSet(resultSet, this,
+			List<T> results =  objectTransformer.createListFromResultSet(resultSet, this,
 					getFirstResult(), count);
+			log.debug("Results transformer returned {} results",results.size());
+			return results;
 
 		} catch (SQLException ex) {
 			ex.printStackTrace();
@@ -61,7 +69,11 @@ public abstract class AbstractSqlQueryDataset<T> extends AbstractQueryDataset<T>
 			ResultSet rs = statement.executeQuery();
 
 			if (rs.next()) {
-				return rs.getInt(1);
+			int value = rs.getInt(1);
+				log.debug("Fetch Result Count SQL returned {}",value);
+				return value;
+			} else {
+				log.error("Fetch Result Count SQL returned no rows!");
 			}
 		} catch (SQLException ex) {
 			ex.printStackTrace();
@@ -71,13 +83,22 @@ public abstract class AbstractSqlQueryDataset<T> extends AbstractQueryDataset<T>
 
 	private PreparedStatement buildPreparedStatement(String selectSql,boolean includeOrderBy)
 			throws SQLException {
+		
 		RestrictionBuilder rb = new RestrictionBuilder(this,
 				ParameterStyle.ORDERED_QUESTION_MARKS);
 		String sql = selectSql + rb.buildWhereClause();
 		if (includeOrderBy) {
 			sql = sql + calculateOrderByClause();
 		}
+		log.debug("Building statement as : {}",sql);
 		PreparedStatement statement = connection.prepareStatement(sql);
+		List<Parameter> params = rb.getParameterList();
+		for (int i = 0;i < params.size();i++) {
+			Parameter param = params.get(i);			
+			log.debug("Setting parameter {} to '{}'",Integer.valueOf(i),param.getValue());
+			statement.setObject(i+1, param.getValue());
+		}
+		
 		return statement;
 	}
 

a86a39bf6a089ac2329a73d72becae29f2bd3528
Andy Gibson <contact@andygibson.net>
2009-08-19 18:12:33 +0000

    Changed references to sql dataset to jdbc dataset

--- samples/wicketDemo/src/main/java/org/jdataset/sample/wicket/HeaderLinkPanel.java
+++ samples/wicketDemo/src/main/java/org/jdataset/sample/wicket/HeaderLinkPanel.java
@@ -5,7 +5,7 @@ import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.jdataset.sample.wicket.custom.FileCustomPaginatorPage;
 import org.jdataset.sample.wicket.custom.JpaCustomPaginatorPage;
-import org.jdataset.sample.wicket.custom.SqlCustomPaginatorPage;
+import org.jdataset.sample.wicket.custom.JdbcCustomPaginatorPage;
 import org.jdataset.sample.wicket.dataprovider.JpaDataProviderPage;
 import org.jdataset.sample.wicket.dataprovider.SqlDataProviderPage;
 import org.jdataset.sample.wicket.search.JpaSearchPage;
@@ -19,7 +19,7 @@ public class HeaderLinkPanel extends Panel {
 		super(id);
 				
 		add(new BookmarkablePageLink<WebPage>("jpaDatasetLink",JpaCustomPaginatorPage.class));
-		add(new BookmarkablePageLink<WebPage>("sqlDatasetLink",SqlCustomPaginatorPage.class));
+		add(new BookmarkablePageLink<WebPage>("jdbcDatasetLink",JdbcCustomPaginatorPage.class));
 		add(new BookmarkablePageLink<WebPage>("fileDatasetLink",FileCustomPaginatorPage.class));
 		add(new BookmarkablePageLink<WebPage>("jpaProviderLink",JpaDataProviderPage.class));
 		add(new BookmarkablePageLink<WebPage>("sqlProviderLink",SqlDataProviderPage.class));

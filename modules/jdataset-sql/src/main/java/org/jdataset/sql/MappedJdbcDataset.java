2df16d0a403eb977978368274b63f97e44e850e9
Andy Gibson <contact@andygibson.net>
2009-08-20 20:04:36 +0000

    Added comments

--- modules/jdataset-sql/src/main/java/org/jdataset/sql/MappedJdbcDataset.java
+++ modules/jdataset-sql/src/main/java/org/jdataset/sql/MappedJdbcDataset.java
@@ -4,12 +4,38 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.jdataset.QueryDataset;
+
 /**
  * Implementation of an {@link AbstractJdbcDataset} that returns the results as
  * a set of {@link TableRow} objects that contain the columns as map values.
+ * <p>
+ * Since this is not a structured {@link QueryDataset} you must provide the
+ * select, where and order by clauses manually in the
+ * {@link #setSelectStatement(String)} and {@link #setCountStatement(String)}
+ * statements. To use a {@link QueryDataset} based query, you can use the
+ * {@link MappedJdbcQueryDataset} dataset instead.
  * 
  * @author Andy Gibson
  * 
+ *         <pre>
+ * MappedJdbcDataset ds  new MappedJdbcDataset(connection);
+ * ds.setSelect(&quot;select * from Person p where p.lastName = :lastNameParam order by firstName&quot;);
+ * ds.setCount(&quot;select count(1) from Person p where p.lastName = :lastNameParam&quot;);
+ * ds.addParameter(&quot;lastNameParam&quot;,&quot;SMITH&quot;);
+ * 
+ * int count = ds.getResultCount();
+ * List&lt;TableRow&gt; results = ds.getResultList();
+ * assertEqual(count,results.size());
+ *  
+ * //print out each row
+ * for (TableRow row : ds) {
+ *     log.debug(row.getValue(&quot;id&quot;)+&quot; , &quot; +  row.getValue(&quot;phone&quot;));
+ * }
+ * 
+ * </pre>
+ * 
+ * @see MappedJdbcQueryDataset
  */
 public class MappedJdbcDataset extends AbstractJdbcDataset<TableRow> {
 
--- modules/jdataset-sql/src/main/java/org/jdataset/sql/MappedJdbcQueryDataset.java
+++ modules/jdataset-sql/src/main/java/org/jdataset/sql/MappedJdbcQueryDataset.java
@@ -5,10 +5,39 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 
-public class MappedJdbcQueryDataset extends AbstractJdbcQueryDataset<TableRow>{
+import org.jdataset.QueryDataset;
+
+/**
+ * Implementation of an {@link AbstractJdbcQueryDataset} that returns the
+ * results as a set of {@link TableRow} objects that contain the columns as map
+ * values. Since this is implements the {@link QueryDatase} interface, you can
+ * apply parameterized restrictions and ordering to the query. The select/count
+ * statement should contain only the select part and the dataset will add the
+ * rest.
+ * 
+ * <pre>
+ * MappedJdbcQueryDataset ds  new MappedJdbcQueryDataset(connection);
+ * ds.setSelect("select * from Person p");
+ * ds.setCount("select count(1) from Person p");
+ * ds.addRestriction("p.id = :id");
+ * ds.addRestriction("p.firstName like :firstName");
+ * ds.addParameter("id",getId());
+ * ds.addParameter("firstName",firstName+"%");
+ * 
+ * int count = ds.getResultCount();
+ * List<TableRow> results = ds.getResultList();
+ * log.debug("first Result id = "+results.get(0).getValue("id"));
+ * 
+ * </pre>
+ * 
+ * 
+ * @author Andy Gibson
+ * 
+ */
+public class MappedJdbcQueryDataset extends AbstractJdbcQueryDataset<TableRow> {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	public MappedJdbcQueryDataset() {
 		super();
 	}
@@ -22,11 +51,12 @@ public class MappedJdbcQueryDataset extends AbstractJdbcQueryDataset<TableRow>{
 			throws SQLException {
 		TableRow result = new TableRow();
 		ResultSetMetaData metadata = resultSet.getMetaData();
-		
-		for (int i = 0;i < metadata.getColumnCount();i++) {
-			result.add(metadata.getColumnName(i+1), resultSet.getObject(i+1));
+
+		for (int i = 0; i < metadata.getColumnCount(); i++) {
+			result.add(metadata.getColumnName(i + 1), resultSet
+					.getObject(i + 1));
 		}
-		
+
 		return result;
 	}
 

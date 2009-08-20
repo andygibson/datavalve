aa78148c898cfa97eda3ab0e2e1f8d8ca75dffb8
Andy Gibson <contact@andygibson.net>
2009-08-20 17:07:34 +0000

    Removed some superfluous code in the extract parameters method

--- modules/jdataset-core/src/main/java/org/jdataset/RegexParameterParser.java
+++ modules/jdataset-core/src/main/java/org/jdataset/RegexParameterParser.java
@@ -6,8 +6,16 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class RegexParameterParser implements ParameterParser,Serializable {
-	
+/**
+ * Splits the query line based on a regular expression. Override
+ * {@link #getRegEx()} to change the regex used to find parameters. The default
+ * is to split based on <code>:name"</code> or <code>#{name}"</code>.
+ * 
+ * @author Andy Gibson
+ * 
+ */
+public class RegexParameterParser implements ParameterParser, Serializable {
+
 	private Pattern pattern;
 
 	protected Pattern getPattern() {
@@ -16,26 +24,20 @@ public class RegexParameterParser implements ParameterParser,Serializable {
 		}
 		return pattern;
 	}
-	
+
 	private String getRegEx() {
 		return ":[A-Za-z0-9]+|#\\{.+}";
 	}
 
 	public String[] extractParameters(String line) {
-		List<String> parameters = new ArrayList<String>();
-		String[] results = new String[0];
-		
+		List<String> parameters = new ArrayList<String>();		
+
 		Matcher matcher = getPattern().matcher(line);
 		while (matcher.find()) {
 			parameters.add(matcher.group());
 		}
-
-		// if we have params, push them into the array, otherwise return the
-		// empty array we created
-		if (parameters.size() != 0) {
-			results = parameters.toArray(results);
-		}
-		return results;
+		// if we have params, return them as an array
+		return parameters.toArray(new String[parameters.size()]);
 	}
 
 }

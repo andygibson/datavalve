10a98d12b4dee38b0c0e7da1fd958a1ad6c5d2a8
Andy Gibson <contact@andygibson.net>
2009-08-20 17:38:32 +0000

    Added comments

--- modules/jdataset-core/src/main/java/org/jdataset/ParameterParser.java
+++ modules/jdataset-core/src/main/java/org/jdataset/ParameterParser.java
@@ -1,7 +1,16 @@
 package org.jdataset;
 
+/**
+ * Simple interface for defining a parameter parser that can take a line of a
+ * query language and extract the parameters as an array of String. Any
+ * implementations should be aware of the different types of parameter syntax
+ * that might be used.
+ * 
+ * @author Andy Gibson
+ * 
+ */
 public interface ParameterParser {
-	
+
 	String[] extractParameters(String line);
 
 }

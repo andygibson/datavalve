49820827c01f065c356b4649356b807f4019ab4d
Andy Gibson <contact@andygibson.net>
2009-08-20 17:00:20 +0000

    This interface now extends the statement dataset and gets the get/set count/select statement methods from there also.

--- modules/jdataset-api/src/main/java/org/jdataset/QueryDataset.java
+++ modules/jdataset-api/src/main/java/org/jdataset/QueryDataset.java
@@ -3,37 +3,27 @@ package org.jdataset;
 import java.util.List;
 import java.util.Map;
 
-import org.jdataset.ParameterizedDataset;
-
 /**
- * The <code>QueryDataset</code> interface extends the {@link ParameterizedDataset}
- * interface and adds methods for handling select and count text statements that
- * may be used for SQL or EJBQL, as well as textual restrictions and mapping
- * order key values to field values.
+ * The <code>QueryDataset</code> interface extends the {@link StatementDataset}
+ * interface and adds methods for handling restrictions and mapping order key
+ * values to field values.
  * <p>
- * This interface can be used to interact with a dataset that contains some kind
- * of querying for count and selection, where the order key maps to a different
- * representation, and where there are zero or more text defined restrictions.
+ * This interface can be used to implement with a dataset that contains a
+ * mechanism for querying count and selection results. For these datasets the
+ * order key maps to a different dataset specific representation, and there are
+ * zero or more text defined restrictions.
  * 
  * @author Andy Gibson
  * 
  * @param <T>
  *            Object type this dataset contains
  */
-public interface QueryDataset<T> extends ParameterizedDataset<T> {
-
-	public String getSelectStatement();
-
-	public void setSelectStatement(String selectStatement);
-
-	public String getCountStatement();
-
-	public void setCountStatement(String countStatement);
+public interface QueryDataset<T> extends StatementDataset<T> {
 
 	public Map<String, String> getOrderKeyMap();
 
 	public void setOrderKeyMap(Map<String, String> orderKeyMap);
-	
+
 	public void addRestriction(String restriction);
 
 	public List<String> getRestrictions();

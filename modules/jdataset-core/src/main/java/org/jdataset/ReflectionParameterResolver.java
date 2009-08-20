39a1e89d0a6dc70be09e395a38b2f6800e958988
Andy Gibson <contact@andygibson.net>
2009-08-20 17:06:02 +0000

    We pass in a ParameterizedDataset type instead of an ObjectDataset type

--- modules/jdataset-core/src/main/java/org/jdataset/ReflectionParameterResolver.java
+++ modules/jdataset-core/src/main/java/org/jdataset/ReflectionParameterResolver.java
@@ -39,7 +39,7 @@ public class ReflectionParameterResolver implements ParameterResolver,Serializab
 		return object;
 	}
 
-	public boolean resolveParameter(ObjectDataset dataset,Parameter parameter) {
+	public boolean resolveParameter(ParameterizedDataset<? extends Object> dataset,Parameter parameter) {
 		String name=parameter.getName().substring(1);//remove the starting ':'
 		
 		String[] properties = name.split("\\.");

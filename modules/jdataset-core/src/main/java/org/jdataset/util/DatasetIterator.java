c3454969da3a9a514095507db3682b17b33924db
Andy Gibson <contact@andygibson.net>
2009-08-20 17:53:53 +0000

    Added comments

--- modules/jdataset-core/src/main/java/org/jdataset/util/DatasetIterator.java
+++ modules/jdataset-core/src/main/java/org/jdataset/util/DatasetIterator.java
@@ -4,6 +4,12 @@ import java.util.Iterator;
 
 import org.jdataset.ObjectDataset;
 
+/**
+ * Iterator implementation for the iterator that is returned from the 
+ * @author Andy Gibson
+ *
+ * @param <T> Type of object this iterator returns
+ */
 public class DatasetIterator<T> implements Iterator<T> {
 
 	private static final long serialVersionUID = 1L;

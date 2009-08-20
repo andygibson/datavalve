04f6935115072ac26926fc445be3bfa7ed265da7
Andy Gibson <contact@andygibson.net>
2009-08-20 18:06:16 +0000

    Added class comment

--- modules/jdataset-core/src/main/java/org/jdataset/util/SourcedDataset.java
+++ modules/jdataset-core/src/main/java/org/jdataset/util/SourcedDataset.java
@@ -5,11 +5,24 @@ import java.util.List;
 import org.jdataset.AbstractDataset;
 import org.jdataset.ObjectDataset;
 
+/**
+ * Provides a mechanism for having multiple {@link ObjectDataset} instances
+ * references the same core data to allow for 2 different 'cursors' to the same
+ * data. This can be especially useful with in-memory datasets that you want to
+ * display in multiple places. This way, you can have one instance of the data
+ * and multiple readers of the data. You can even share the dataset in the
+ * application scope since the read operations on the wrapped dataset are thread
+ * safe.
+ * 
+ * @author Andy Gibson
+ * 
+ * @param <T> Type of object this dataset represents
+ */
 public class SourcedDataset<T> extends AbstractDataset<T> {
 
 	private final ObjectDataset<T> dataset;
 	private Boolean nextAvailable;
-	
+
 	public SourcedDataset(ObjectDataset<T> dataset) {
 		super();
 		this.dataset = dataset;
@@ -42,12 +55,11 @@ public class SourcedDataset<T> extends AbstractDataset<T> {
 		}
 		return nextAvailable;
 	}
-	
+
 	@Override
 	public void invalidateResults() {
 		super.invalidateResults();
 		nextAvailable = null;
 	}
-	
 
 }

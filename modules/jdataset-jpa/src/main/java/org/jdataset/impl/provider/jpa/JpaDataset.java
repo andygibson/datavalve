6f11577c160a53e3ae31cda624f3b67bb1258fbb
Andy Gibson <contact@andygibson.net>
2010-02-08 21:55:07 +0000

    Added javadoc

--- modules/jdataset-jpa/src/main/java/org/jdataset/impl/provider/jpa/JpaDataset.java
+++ modules/jdataset-jpa/src/main/java/org/jdataset/impl/provider/jpa/JpaDataset.java
@@ -3,7 +3,17 @@ package org.jdataset.impl.provider.jpa;
 import org.jdataset.dataset.GenericProviderDataset;
 import org.jdataset.provider.QueryDataProvider;
 
-public class JpaDataset<T> extends GenericProviderDataset<T, QueryDataProvider<T>> {
+/**
+ * Creates a helper class that encapsulates the Query base dataset with a an
+ * {@link AbstractJpaDataProvider}.
+ * 
+ * @author Andy Gibson
+ * 
+ * @param <T>
+ *            The type of object this dataset returns
+ */
+public class JpaDataset<T> extends
+		GenericProviderDataset<T, QueryDataProvider<T>> {
 
 	private static final long serialVersionUID = 1L;
 

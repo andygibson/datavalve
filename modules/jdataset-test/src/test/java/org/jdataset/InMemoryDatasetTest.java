60b0e6feb246112980ecb8a9af1fa5eaf150cd56
Andy Gibson <contact@andygibson.net>
2010-03-12 15:58:25 +0000

    Changed the unbound ordering test to what it should be.

--- modules/jdataset-test/src/test/java/org/jdataset/InMemoryDatasetTest.java
+++ modules/jdataset-test/src/test/java/org/jdataset/InMemoryDatasetTest.java
@@ -10,178 +10,182 @@ import org.jdataset.dataset.ObjectDataset;
 import org.jdataset.impl.InMemoryDataProvider;
 import org.jdataset.testing.junit.AbstractObjectDatasetJUnitTest;
 
-public class InMemoryDatasetTest extends AbstractObjectDatasetJUnitTest<Integer> implements Serializable {
+public class InMemoryDatasetTest extends
+		AbstractObjectDatasetJUnitTest<Integer> implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
-    private static Logger log = Logger.getLogger(InMemoryDatasetTest.class.getName());
-    
-    
 
-    public ObjectDataset<Integer> buildTestDataset() {
-    	
-    	return buildTestDataset(100);
-    }
-    
-    public ObjectDataset<Integer> buildTestDataset(final int count) {
-    	
-        InMemoryDataProvider<Integer> ds = new InMemoryDataProvider<Integer>() {
+	private static Logger log = Logger.getLogger(InMemoryDatasetTest.class
+			.getName());
 
-        	private static final long serialVersionUID = 1L;
-        	
-            @Override
-            protected List<Integer> fetchBackingData() {
-                List<Integer> res = new ArrayList<Integer>();
-                for (int i = 0; i < count; i++) {
-                    res.add(i);
-                }
-                return res;
-            }
-        };
-        
-        return new GenericProviderDataset<Integer, InMemoryDataProvider<Integer>>(ds);
-    }
+	public ObjectDataset<Integer> buildTestDataset() {
 
-    public void testGetResultCount() {
-    	ObjectDataset<Integer> ds = buildTestDataset();
-        int res = ds.getResultCount();
-        assertEquals(100, res);
-    }
+		return buildTestDataset(100);
+	}
 
-    public void testGetResultsUnbound() {
-    	ObjectDataset<Integer> ds = buildTestDataset();
-        List<Integer> res = ds.getResultList();
-        
-        assertNotNull(res);
-        assertEquals(100, res.size());
-        for (int i = 0; i < 10; i++) {
-            assertEquals(i, res.get(i).intValue());
-        }
-    }
-    
-    public void testGetResultsUnboundOrdering() {
-    	ObjectDataset<Integer> ds = buildTestDataset();
-        List<Integer> res = ds.getResultList();
-        
-        assertNotNull(res);
-        assertEquals(100, res.size());
-        for (int i = 0; i < 10; i++) {
-            assertEquals(i, res.get(i).intValue());
-        }
-        
-        //lets flip the order
-        ds.setOrderAscending(false);
-        res = ds.getResultList();
-        
-        assertNotNull(res);
-        assertEquals(100, res.size());
-        for (int i = 0; i < 10; i++) {
-            assertEquals(99-i, res.get(i).intValue());
-        }
-        
-    }
-    
+	public ObjectDataset<Integer> buildTestDataset(final int count) {
 
-    public void testGetResultsBound() {
-    	ObjectDataset<Integer> ds = buildTestDataset();
-        ds.setMaxRows(12);
-        List<Integer> res = ds.getResultList();
+		InMemoryDataProvider<Integer> ds = new InMemoryDataProvider<Integer>() {
 
-        assertNotNull(res);
-        assertEquals(12, res.size());
-        for (int i = 0; i < 12; i++) {
-            assertEquals(i, res.get(i).intValue());
-        }
-    }
+			private static final long serialVersionUID = 1L;
 
-    public void testLast() {
-    	ObjectDataset<Integer> ds = buildTestDataset();
-        ds.setMaxRows(10);
-        ds.last();        
-        assertEquals(10, ds.getPageCount());
-        int firstResult = ds.getFirstResult();
-        log.fine("First result = "+firstResult);        
-        assertEquals(90, firstResult);
-    }
-    
-    public void testLastWithFractionalCount() {
-    	ObjectDataset<Integer> ds = buildTestDataset(95);
-        ds.setMaxRows(10);
-        ds.last();
-        Integer firstResult = ds.getFirstResult();        
-        assertNotNull(firstResult);
-        assertEquals(90, firstResult.longValue());
-    }
-    
-    public void testLastWithNoPaging() {
-    	ObjectDataset<Integer> ds = buildTestDataset(5);
-        ds.setMaxRows(10);
-        ds.last();
-        int firstResult = ds.getFirstResult();
-        log.fine("First result = "+firstResult);        
-        assertEquals(0, firstResult);
-    }
-    
-    public void testPaginationPreviousAllResults() {
-    	ObjectDataset<Integer> ds = buildTestDataset(100);
-        assertEquals(false, ds.isPreviousAvailable());
-    }
-    
-    public void testPaginationNextAllResults() {
-    	ObjectDataset<Integer> ds = buildTestDataset(100);
-        assertEquals(false, ds.isNextAvailable());
-    }
-    
-    public void testPaginationPreviousPaged() {
-    	ObjectDataset<Integer> ds = buildTestDataset(100);
-        ds.setMaxRows(10);
-        assertEquals(false, ds.isPreviousAvailable());
-    }
-    
-    public void testPaginationNextPaged() {
-    	ObjectDataset<Integer> ds = buildTestDataset(100);
-        ds.setMaxRows(10);
-        assertEquals(true, ds.isNextAvailable());
-    }
-    
-    public void testPaginationNextNoRead() {
-    	ObjectDataset<Integer> ds = buildTestDataset(100);
-        ds.setMaxRows(10);
-        ds.next();
-        assertEquals(true, ds.isNextAvailable());
-        assertEquals(true, ds.isPreviousAvailable());
-        
-    }
-    
-    public void testPaginationPreviousNoRead() {
-        ObjectDataset<Integer> ds = buildTestDataset(100);
-        ds.setMaxRows(10);
-        ds.previous();
-        assertEquals(0,ds.getFirstResult());
-        assertEquals(1, ds.getPage());
-        assertEquals(true, ds.isNextAvailable());
-        assertEquals(false, ds.isPreviousAvailable());        
-    }
-    
-    public void testPaginationLastNoRead() {
-    	ObjectDataset<Integer> ds = buildTestDataset(100);
-        ds.setMaxRows(10);
-        ds.last();
-        assertEquals(false, ds.isNextAvailable());
-        assertEquals(true, ds.isPreviousAvailable());        
-    }
-    
-    public void testSmalldataset() {
-    	ObjectDataset<Integer> ds = buildTestDataset(6);
-        ds.setMaxRows(10);        
-        assertEquals(false, ds.isNextAvailable());
-        assertEquals(false, ds.isPreviousAvailable());
-        ds.next();
-        assertEquals(6, ds.getResultCount().intValue());
-        assertEquals(false, ds.isNextAvailable());
-        assertEquals(false, ds.isPreviousAvailable());
-        
-    }
+			@Override
+			protected List<Integer> fetchBackingData() {
+				List<Integer> res = new ArrayList<Integer>();
+				for (int i = 0; i < count; i++) {
+					res.add(i);
+				}
+				return res;
+			}
+		};
+
+		return new GenericProviderDataset<Integer, InMemoryDataProvider<Integer>>(
+				ds);
+	}
+
+	public void testGetResultCount() {
+		ObjectDataset<Integer> ds = buildTestDataset();
+		int res = ds.getResultCount();
+		assertEquals(100, res);
+	}
+
+	public void testGetResultsUnbound() {
+		ObjectDataset<Integer> ds = buildTestDataset();
+		List<Integer> res = ds.getResultList();
+
+		assertNotNull(res);
+		assertEquals(100, res.size());
+		for (int i = 0; i < 10; i++) {
+			assertEquals(i, res.get(i).intValue());
+		}
+	}
+
+	/**
+	 * Tests that ordering does not change when just the order is
+	 * changed
+	 */
+	public void testGetResultsUnboundOrdering() {
+		ObjectDataset<Integer> ds = buildTestDataset();
+		List<Integer> original = ds.getResultList();
+		ds.setOrderAscending(false);
+		List<Integer> list1 = ds.getResultList();
+		ds.setOrderAscending(true);
+		List<Integer> list2 = ds.getResultList();
+
+		assertNotNull(original);
+		assertNotNull(list1);
+		assertNotNull(list2);
+		assertEquals(100, original.size());
+		assertEquals(100, list1.size());
+		assertEquals(100, list2.size());
+		
+		for (int i = 0; i < 10; i++) {
+			assertEquals(i, original.get(i).intValue());
+			assertEquals(original.get(i).intValue(), list1.get(i).intValue());
+			assertEquals(original.get(i).intValue(), list2.get(i).intValue());
+		}
+	}
+
+	public void testGetResultsBound() {
+		ObjectDataset<Integer> ds = buildTestDataset();
+		ds.setMaxRows(12);
+		List<Integer> res = ds.getResultList();
+
+		assertNotNull(res);
+		assertEquals(12, res.size());
+		for (int i = 0; i < 12; i++) {
+			assertEquals(i, res.get(i).intValue());
+		}
+	}
+
+	public void testLast() {
+		ObjectDataset<Integer> ds = buildTestDataset();
+		ds.setMaxRows(10);
+		ds.last();
+		assertEquals(10, ds.getPageCount());
+		int firstResult = ds.getFirstResult();
+		log.fine("First result = " + firstResult);
+		assertEquals(90, firstResult);
+	}
+
+	public void testLastWithFractionalCount() {
+		ObjectDataset<Integer> ds = buildTestDataset(95);
+		ds.setMaxRows(10);
+		ds.last();
+		Integer firstResult = ds.getFirstResult();
+		assertNotNull(firstResult);
+		assertEquals(90, firstResult.longValue());
+	}
+
+	public void testLastWithNoPaging() {
+		ObjectDataset<Integer> ds = buildTestDataset(5);
+		ds.setMaxRows(10);
+		ds.last();
+		int firstResult = ds.getFirstResult();
+		log.fine("First result = " + firstResult);
+		assertEquals(0, firstResult);
+	}
+
+	public void testPaginationPreviousAllResults() {
+		ObjectDataset<Integer> ds = buildTestDataset(100);
+		assertEquals(false, ds.isPreviousAvailable());
+	}
+
+	public void testPaginationNextAllResults() {
+		ObjectDataset<Integer> ds = buildTestDataset(100);
+		assertEquals(false, ds.isNextAvailable());
+	}
+
+	public void testPaginationPreviousPaged() {
+		ObjectDataset<Integer> ds = buildTestDataset(100);
+		ds.setMaxRows(10);
+		assertEquals(false, ds.isPreviousAvailable());
+	}
+
+	public void testPaginationNextPaged() {
+		ObjectDataset<Integer> ds = buildTestDataset(100);
+		ds.setMaxRows(10);
+		assertEquals(true, ds.isNextAvailable());
+	}
+
+	public void testPaginationNextNoRead() {
+		ObjectDataset<Integer> ds = buildTestDataset(100);
+		ds.setMaxRows(10);
+		ds.next();
+		assertEquals(true, ds.isNextAvailable());
+		assertEquals(true, ds.isPreviousAvailable());
+
+	}
+
+	public void testPaginationPreviousNoRead() {
+		ObjectDataset<Integer> ds = buildTestDataset(100);
+		ds.setMaxRows(10);
+		ds.previous();
+		assertEquals(0, ds.getFirstResult());
+		assertEquals(1, ds.getPage());
+		assertEquals(true, ds.isNextAvailable());
+		assertEquals(false, ds.isPreviousAvailable());
+	}
+
+	public void testPaginationLastNoRead() {
+		ObjectDataset<Integer> ds = buildTestDataset(100);
+		ds.setMaxRows(10);
+		ds.last();
+		assertEquals(false, ds.isNextAvailable());
+		assertEquals(true, ds.isPreviousAvailable());
+	}
+
+	public void testSmalldataset() {
+		ObjectDataset<Integer> ds = buildTestDataset(6);
+		ds.setMaxRows(10);
+		assertEquals(false, ds.isNextAvailable());
+		assertEquals(false, ds.isPreviousAvailable());
+		ds.next();
+		assertEquals(6, ds.getResultCount().intValue());
+		assertEquals(false, ds.isNextAvailable());
+		assertEquals(false, ds.isPreviousAvailable());
+
+	}
 
 	@Override
 	public ObjectDataset<Integer> buildObjectDataset() {
@@ -192,6 +196,5 @@ public class InMemoryDatasetTest extends AbstractObjectDatasetJUnitTest<Integer>
 	public int getDataRowCount() {
 		return 100;
 	}
-    
-    
+
 }

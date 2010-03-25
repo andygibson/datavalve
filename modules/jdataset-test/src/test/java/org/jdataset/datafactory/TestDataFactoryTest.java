95678950da3d4c31dffebc980e5dd7310e98c3bb
Andy Gibson <contact@andygibson.net>
2010-03-25 19:29:38 +0000

    Updated, added words for word generation

---
+++ modules/jdataset-test/src/test/java/org/jdataset/datafactory/TestDataFactoryTest.java
@@ -0,0 +1,24 @@
+package org.jdataset.datafactory;
+
+import org.jdataset.testing.TestDataFactory;
+
+import junit.framework.TestCase;
+
+public class TestDataFactoryTest extends TestCase {
+
+	public void testRandomText() {
+
+		for (int i = 0; i < 1000; i++) {
+
+			String ret = TestDataFactory.getRandomText(200, 20);
+			System.out.println(ret);
+			System.out.println("");
+
+			assertTrue("Length is " + ret.length(), ret.length() >= 20);
+			assertTrue("Length is " + ret.length(), ret.length() <= 200);
+
+		}
+
+	}
+
+}

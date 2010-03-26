2b03e5d8dc1f279ae23867c4b45bb846c9232312
Andy Gibson <contact@andygibson.net>
2010-03-26 20:41:58 +0000

    Made changes to the final application, CSS styling, removed info box from the layout, changed the images to be PNGs, added page number etc.

--- samples/cdidemo/src/main/java/org/fluttercode/spigot/jsfdemo/PersonSearchDataset.java
+++ samples/cdidemo/src/main/java/org/fluttercode/spigot/jsfdemo/PersonSearchDataset.java
@@ -1,18 +1,18 @@
 /*
-* Copyright 2010, Andrew M Gibson
-*
-* www.andygibson.net
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-* http://www.apache.org/licenses/LICENSE-2.0
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2010, Andrew M Gibson
+ *
+ * www.andygibson.net
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package org.fluttercode.spigot.jsfdemo;
 
@@ -36,21 +36,24 @@ public class PersonSearchDataset extends QueryDataset<Person> {
 
 	private static final long serialVersionUID = 1L;
 
-	public PersonSearchDataset() {		
-		super();		
+	// constructor required for CDI proxybility
+	public PersonSearchDataset() {
+		super();
 	}
 
 	@Inject
 	public PersonSearchDataset(PersonSearchProvider provider) {
 		super(provider);
-		setMaxRows(10);			
+		setOrderKey("id");
+		setMaxRows(10);
 	}
 
 	@Override
 	@Produces
 	@Named("personResults")
 	public List<Person> getResultList() {
+		// this function is overridden so we can mark it as a producer that
+		// produces the results
 		return super.getResultList();
-
-	}	
+	}
 }

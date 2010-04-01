ca3651ea6521a9567b1ddaafd9f7c7b05030c95d
Andy Gibson <contact@andygibson.net>
2010-04-01 18:03:37 +0000

    Fixed test cases so we don't use the 'bad' method of iterating across data.

--- samples/examples/src/test/java/org/fluttercode/spigot/samples/examples/IntegerDataProviderTest.java
+++ samples/examples/src/test/java/org/fluttercode/spigot/samples/examples/IntegerDataProviderTest.java
@@ -1,18 +1,18 @@
 /*
-* Copyright 2010, Andrew M Gibson
-*
-* www.andygibson.net
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-* http://www.apache.org/licenses/LICENSE-2.0
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2010, Andrew M Gibson
+ *
+ * www.andygibson.net
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package org.fluttercode.spigot.samples.examples;
 
@@ -33,11 +33,11 @@ public class IntegerDataProviderTest extends
 		AbstractObjectDatasetJUnitTest<Integer> {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Override
 	public ObjectDataset<Integer> buildObjectDataset() {
-		IntegerDataProvider provider = new IntegerDataProvider();		
-		return new Dataset<Integer,IntegerDataProvider>(provider);
+		IntegerDataProvider provider = new IntegerDataProvider();
+		return new Dataset<Integer, IntegerDataProvider>(provider);
 	}
 
 	@Override
@@ -47,26 +47,11 @@ public class IntegerDataProviderTest extends
 
 	@Override
 	public boolean includeSerializationTest() {
-	
+
 		return true;
 	}
-	
+
 	public void testResultsPaginated() {
-		ObjectDataset<Integer> ds = buildObjectDataset();
-		ds.setMaxRows(10);
-		Integer expected = 1;
-		while (ds.isNextAvailable()) {
-			List<Integer> results = ds.getResultList();
-			for (Integer i : results) {
-				assertEquals(expected, i);
-				expected = expected + 1;
-			}
-			ds.next();
-		}
-
-	}
-
-	public void testResultsIterated() {
 		ObjectDataset<Integer> ds = buildObjectDataset();
 		ds.setMaxRows(10);
 		Integer expected = 1;
@@ -78,15 +63,11 @@ public class IntegerDataProviderTest extends
 
 	public void testResultsNonPaginated() {
 		ObjectDataset<Integer> ds = buildObjectDataset();
-		ds.setMaxRows(null);
 		Integer expected = 1;
-
-		List<Integer> results = ds.getResultList();
-		assertEquals(100, results.size()); //check size
-		for (Integer i : results) {
+		for (Integer i : ds) {
 			assertEquals(expected, i);
 			expected = expected + 1;
 		}
-	}	
-	
+	}
+
 }

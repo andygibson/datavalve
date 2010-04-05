1e49b36dcfdbe8e756697c4baa7770d0818e8de5
Andy Gibson <contact@andygibson.net>
2010-04-05 15:57:15 +0000

    Fixed with updated from the jpa seam adapter.

--- modules/spigot-seam/src/main/java/org/fluttercode/spigot/provider/seam/util/SeamJpaNativeDatasetAdapter.java
+++ modules/spigot-seam/src/main/java/org/fluttercode/spigot/provider/seam/util/SeamJpaNativeDatasetAdapter.java
@@ -1,23 +1,22 @@
 /*
-* Copyright 2010, Andrew M Gibson
-*
-* www.andygibson.net
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-* http://www.apache.org/licenses/LICENSE-2.0
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2010, Andrew M Gibson
+ *
+ * www.andygibson.net
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package org.fluttercode.spigot.provider.seam.util;
 
-import java.util.List;
-
+import org.fluttercode.spigot.provider.jpa.JpaNativeProvider;
 import org.fluttercode.spigot.provider.seam.SeamJpaNativeDataset;
 
 /**
@@ -28,6 +27,9 @@ import org.fluttercode.spigot.provider.seam.SeamJpaNativeDataset;
  * SeamJpaNativeDataset once your user interface has migrated to the slightly
  * different Spigot interface.
  * 
+ * While the original Seam EntityQuery never supported native queries, no reason
+ * why we shouldn't offer the extension during the transition.
+ * 
  * @author Andy Gibson
  * 
  * @param <T>
@@ -36,9 +38,15 @@ import org.fluttercode.spigot.provider.seam.SeamJpaNativeDataset;
 public class SeamJpaNativeDatasetAdapter<T> extends SeamJpaNativeDataset<T> {
 
 	private static final long serialVersionUID = 1L;
-	
-	public List<T> getResultList() {
-		return super.getResultList();
+
+	public SeamJpaNativeDatasetAdapter() {
+		super();
+		setMaxRows(10);
+	}
+
+	public SeamJpaNativeDatasetAdapter(JpaNativeProvider<T> provider) {
+		super(provider);
+		setMaxRows(10);
 	}
 
 	public int getActivePage() {
@@ -51,5 +59,13 @@ public class SeamJpaNativeDatasetAdapter<T> extends SeamJpaNativeDataset<T> {
 
 	public boolean getNextExists() {
 		return super.isNextAvailable();
+	}
+
+	public void setEjbql(String ejbql) {
+		getProvider().setSelectStatement(ejbql);
+	}
+
+	public void setCountEjbql(String countEjbql) {
+		getProvider().setCountStatement(countEjbql);
 	}
 }

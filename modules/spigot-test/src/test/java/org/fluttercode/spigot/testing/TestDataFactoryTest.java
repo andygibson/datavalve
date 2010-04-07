20fbe8a3044597b3d1a9a4dd15f23b3999c72918
Andy Gibson <contact@andygibson.net>
2010-04-07 15:21:01 +0000

    Changed license to GPL 3.

--- modules/spigot-test/src/test/java/org/fluttercode/spigot/testing/TestDataFactoryTest.java
+++ modules/spigot-test/src/test/java/org/fluttercode/spigot/testing/TestDataFactoryTest.java
@@ -1,9 +1,36 @@
+/*
+ * Copyright 2010, Andrew M Gibson
+ *
+ * www.andygibson.net
+ *
+ * This file is part of Spigot.
+ *
+ * Spigot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Spigot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Spigot.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package org.fluttercode.spigot.testing;
 
 import org.fluttercode.spigot.testing.TestDataFactory;
 
 import junit.framework.TestCase;
 
+/**
+ * @author Andy Gibson
+ * 
+ */
 public class TestDataFactoryTest extends TestCase {
 
 	public void testRandomText() {
